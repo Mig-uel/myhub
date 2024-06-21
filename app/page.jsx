80958e91194a78f44e4be3a6a207751c441f7e7d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-20 21:01:04 -0400

    feat: Add Hero and InfoBoxes components to homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,16 @@
+import Link from 'next/link'
+
+// components
+import Hero from '@/components/hero.component'
+import InfoBoxes from '@/components/info-boxes.component'
+
 const HomePage = () => {
-  return <>homepage</>
+  return (
+    <>
+      <Hero />
+      <InfoBoxes />
+    </>
+  )
 }
 
 export default HomePage
