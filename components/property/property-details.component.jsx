6bd20b6d4e0c1c6b76950ff4ac68522198946e17
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-21 17:44:49 -0400

    refactor: Improve property fetching and display functionality

--- components/property/property-details.component.jsx
+++ components/property/property-details.component.jsx
@@ -17,24 +17,22 @@ const PropertyDetails = ({
     beds,
     baths,
     square_feet,
+    description,
     amenities,
   },
 }) => {
   const ratesDisplay = () => {
     let ratesArr = ['nightly', 'weekly', 'monthly']
-    let ratesKeys = Object.keys(rates)
 
-    console.log(rates)
-    console.log(ratesKeys)
-    console.log(ratesArr)
-
-    return ratesArr.map((rate) => {
+    return ratesArr.map((rate, index) => {
       if (rate in rates) {
-        console.log(rates[rate])
         return (
-          <div className='flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0'>
+          <div
+            className='flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0'
+            key={index}
+          >
             <div className='text-gray-500 mr-2 font-bold'>
-              {rate.toUpperCase()}
+              {rate.slice(0, 1).toLocaleUpperCase().concat(rate.slice(1))}
             </div>
             <div className='text-2xl font-bold text-blue-500'>
               ${rates[rate].toLocaleString(0, 2)}
@@ -42,44 +40,20 @@ const PropertyDetails = ({
           </div>
         )
       } else {
-        console.log(rates[rate])
         return (
-          <div className='flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0'>
-            <div className='text-gray-500 mr-2 font-bold'>{rate}</div>
+          <div
+            className='flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0'
+            key={index}
+          >
+            <div className='text-gray-500 mr-2 font-bold'>
+              {rate.slice(0, 1).toLocaleUpperCase().concat(rate.slice(1))}
+            </div>
             <div className='text-2xl font-bold'>
               <FaTimes className='text-red-700' />
             </div>
           </div>
         )
       }
-    })
-
-    return ratesArr.map((rate) => {
-      return ratesKeys.map((r) => {
-        if (rates.hasOwnProperty(rate)) {
-          // ratesArr = ratesArr.filter((i) => i !== r)
-          console.log(`RATES ARR: [${ratesArr}]`)
-          return (
-            <div className='flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0'>
-              <div className='text-gray-500 mr-2 font-bold'>
-                {r.toUpperCase()}
-              </div>
-              <div className='text-2xl font-bold text-blue-500'>
-                ${rates[r].toLocaleString(0, 2)}
-              </div>
-            </div>
-          )
-        } else {
-          return (
-            <div className='flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0'>
-              <div className='text-gray-500 mr-2 font-bold'>{r}</div>
-              <div className='text-2xl font-bold'>
-                <FaTimes className='text-red-700' />
-              </div>
-            </div>
-          )
-        }
-      })
     })
   }
 
@@ -101,28 +75,10 @@ const PropertyDetails = ({
         </h3>
         <div className='flex flex-col md:flex-row justify-around'>
           {ratesDisplay()}
-          {/* <div className='flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0'>
-            <div className='text-gray-500 mr-2 font-bold'>Nightly</div>
-            <div className='text-2xl font-bold'>
-              {rates.nightly ? (
-                `$${rates.nightly.toLocaleString(0, 2)}`
-              ) : (
-                <FaTimes className='text-red-700' />
-              )}
-            </div>
-          </div>
-          <div className='flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0'>
-            <div className='text-gray-500 mr-2 font-bold'>Weekly</div>
-            <div className='text-2xl font-bold text-blue-500'>$1,100</div>
-          </div>
-          <div className='flex items-center justify-center mb-4 pb-4 md:pb-0'>
-            <div className='text-gray-500 mr-2 font-bold'>Monthly</div>
-            <div className='text-2xl font-bold text-blue-500'>$4,200</div>
-          </div> */}
         </div>
       </div>
 
-      <div className='bg-white p-6 rounded-lg shadow-md mt-6'>
+      <div className='bg-white p-6 rounded-lg shadow-md mt-6 text-center'>
         <h3 className='text-lg font-bold mb-6'>Description & Details</h3>
         <div className='flex justify-center gap-4 text-blue-500 mb-4 text-xl space-x-9'>
           <p>
@@ -139,74 +95,21 @@ const PropertyDetails = ({
             <span className='hidden sm:inline'>sqft</span>
           </p>
         </div>
-        <p className='text-gray-500 mb-4'>
-          This is a beautiful apartment located near the commons
-        </p>
-        <p className='text-gray-500 mb-4'>
-          We have a beautiful apartment located near the commons. It is a 2
-          bedroom apartment with a full kitchen and bathroom. It is available
-          htmlFor weekly or monthly rentals.
-        </p>
+        <p className='text-gray-500 mb-4'>{description}</p>
       </div>
 
       <div className='bg-white p-6 rounded-lg shadow-md mt-6'>
         <h3 className='text-lg font-bold mb-6'>Amenities</h3>
 
-        <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 list-none'>
-          <li>
-            <i className='fas fa-check text-green-600 mr-2 mt-3'></i> Wifi
-          </li>
-          <li>
-            <i className='fas fa-check text-green-600 mr-2 mt-3'></i>
-            Full kitchen
-          </li>
-          <li>
-            <i className='fas fa-check text-green-600 mr-2 mt-3'></i>
-            Washer & Dryer
-          </li>
-          <li>
-            <i className='fas fa-check text-green-600 mr-2 mt-3'></i>
-            Free Parking
-          </li>
-          <li>
-            <i className='fas fa-check text-green-600 mr-2 mt-3'></i>Hot Tub
-          </li>
-          <li>
-            <i className='fas fa-check text-green-600 mr-2 mt-3'></i>
-            24/7 Security
-          </li>
-          <li>
-            <i className='fas fa-check text-green-600 mr-2 mt-3'></i>
-            Wheelchair Accessible
-          </li>
-          <li>
-            <i className='fas fa-check text-green-600 mr-2 mt-3'></i>
-            Elevator Access
-          </li>
-          <li>
-            <i className='fas fa-check text-green-600 mr-2 mt-3'></i>
-            Dishwasher
-          </li>
-          <li>
-            <i className='fas fa-check text-green-600 mr-2 mt-3'></i>
-            Gym/Fitness Center
-          </li>
-          <li>
-            <i className='fas fa-check text-green-600 mr-2 mt-3'></i>Air
-            Conditioning
-          </li>
-          <li>
-            <i className='fas fa-check text-green-600 mr-2 mt-3'></i>
-            Balcony/Patio
-          </li>
-          <li>
-            <i className='fas fa-check text-green-600 mr-2 mt-3'></i>
-            Smart TV
-          </li>
-          <li>
-            <i className='fas fa-check text-green-600 mr-2 mt-3'></i>
-            Coffee Maker
-          </li>
+        <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 list-none space-y-2'>
+          {amenities.map((amenity, index) => {
+            return (
+              <li key={index}>
+                <FaCheck className=' inline-block text-green-600 mr-2' />{' '}
+                {amenity}
+              </li>
+            )
+          })}
         </ul>
       </div>
       <div className='bg-white p-6 rounded-lg shadow-md mt-6'>
