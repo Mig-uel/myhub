a92e6bfe307bff30a6b5fb17e597b6029f103a62
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-21 12:31:42 -0400

    refactor: Update PropertiesPage to fetch properties from API and sort by creation date

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,11 +1,27 @@
 import PropertyCard from '@/components/property-card.component'
-import properties from '@/data/properties.json'
 
-const PropertiesPage = () => {
+const fetchProperties = async () => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`)
+
+    if (!res.ok) throw new Error('Failed to fetch data')
+
+    return res.json()
+  } catch (error) {
+    console.log(error?.message)
+  }
+}
+
+const PropertiesPage = async () => {
+  const properties = await fetchProperties()
+
+  // sort by created at
+  properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+
   return (
     <section className='px-4 py-6'>
       <div className='container-xl lg:container m-auto px-4 py-6'>
-        {properties.length === 0 ? (
+        {!properties || properties?.length === 0 ? (
           <p>No properties found...</p>
         ) : (
           <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
