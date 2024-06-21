b16768c4da031732235933fe82c9cccab19f24b8
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-21 11:40:40 -0400

    refactor: Update import path for colors in page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,12 @@
 import Link from 'next/link'
+import colors from 'colors'
 
 // components
 import Hero from '@/components/hero.component'
 import InfoBoxes from '@/components/info-boxes.component'
 import HomeProperties from '@/components/home-properties.component'
 
-const HomePage = () => {
+const HomePage = async () => {
   return (
     <>
       <Hero />
