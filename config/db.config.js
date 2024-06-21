0f5fb447ada6e194331e4e45ff122568df29efb3
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-21 11:39:39 -0400

    feat: Connect to MongoDB in connectDB function

---
+++ config/db.config.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+let connected = false
+
+export const connectDB = async () => {
+  mongoose.set('strictQuery', true)
+
+  // if db already connected, don't connect again
+  if (connected) {
+    console.log(`ALREADY CONNECTED TO: ${mongoose.connection.name}`)
+    return
+  }
+
+  // connect to mongodb
+  try {
+    await mongoose.connect(process.env.MONGODB_URI)
+    connected = true
+
+    console.log(`MONGODB CONNECTED: ${mongoose.connection.name}`.green.inverse)
+  } catch (error) {
+    connected = false
+    console.log(`${error}`.red.inverse)
+  }
+}
