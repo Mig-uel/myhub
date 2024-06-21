c04a5a57dcc76dc38802d3d167bb597c5b7ec0da
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-21 14:09:52 -0400

    refactor: Update requests.js to include response helper functions

--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,21 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN
 
+// response helper functions
+export const sendJSONWithStatusResponse = (obj, status) => {
+  return new Response(JSON.stringify(obj), { status })
+}
+export const sendErrorJSONWithStatusResponse = ({ message, stack }, status) => {
+  return new Response(
+    JSON.stringify({
+      message: `${message || 'Something went wrong'}`,
+      stack: process.env.NODE_ENV === 'development' ? stack : '🥞',
+      status,
+    }),
+    { status }
+  )
+}
+
+// fetch all properties
 export const fetchProperties = async () => {
   try {
     if (!apiDomain) return []
