ab91380fb0ffbfd227829b4ca209952654427a03
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-20 22:07:28 -0400

    feat: Add HomeProperties component to homepage

--- app/page.jsx
+++ app/page.jsx
@@ -3,12 +3,14 @@ import Link from 'next/link'
 // components
 import Hero from '@/components/hero.component'
 import InfoBoxes from '@/components/info-boxes.component'
+import HomeProperties from '@/components/home-properties.component'
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   )
 }
