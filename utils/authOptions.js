605f2224bd5b7f193b2acb8af6fd2088fc84e8d1
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-22 15:18:22 -0400

    refactor: Add user creation logic to signIn function

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,3 +1,5 @@
+import { connectDB } from '@/config/db.config'
+import User from '@/models/user.model'
 import GoogleProvider from 'next-auth/providers/google'
 
 // from next-auth documentation
@@ -22,16 +24,36 @@ export const authOptions = {
     // invoke on successful sign in
     async signIn({ profile }) {
       // 1. connect to db
+      await connectDB()
+
       // 2. check if user exists
+      const userExists = await User.findOne({ email: profile.email })
+
       // 3. if not, then add user to db
+      if (!userExists) {
+        const username = profile.name.slice(0, 20)
+
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        })
+      }
+
       // 4. return true to allow sign in
+      return true
     },
 
     // modifies the session object
     async session({ session }) {
       // 1. get user from db
+      const user = await User.findOne({ email: session.user.email })
+
       // 2. assign the user id to the session
+      session.user.id = user._id.toString()
+
       // 3. return session
+      return session
     },
   },
 }
